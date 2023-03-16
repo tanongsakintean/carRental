805f7c97affa13d32eaabf1a9c3b1d2ea7b5b8df
iconC4 <cfour2545@gmail.com>
2023-03-16 20:21:56 +0700

    Up Up

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -1,7 +1,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../index.php" class="brand-link">
-        <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
@@ -47,6 +48,22 @@
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-header">EXAMPLES</li>
+                <li class="nav-item">
+                    <a href="?p=form_member" class="nav-link">
+                        <i class="nav-icon fas fa-table"></i>
+                        <p>
+                            แบบฟอร์มเพิ่มสมาชิก
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+
+                </li>
+
+            </ul>
+        </nav>
     </div>
     <!-- /.sidebar -->
 </aside>
